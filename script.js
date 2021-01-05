9eee17b7e3e1a7eb9ac1cd5ba901c3c90b59d071
prashant462 <prashantmalhotra462@gmail.com>
2021-01-05 20:49:26 +0530

    js updated

---
+++ script.js
@@ -0,0 +1,42 @@
+let loc=document.getElementById("location");
+let tempicon=document.getElementById("temp-icon");
+let tempvalue=document.getElementById("temp-value");
+let climate=document.getElementById("climate");
+let iconfile;
+const searchInput=document.getElementById("search-input");
+const searchButton=document.getElementById("search-button");
+const proxy="https://cors-anywhere.herokuapp.com/";
+
+window.addEventListener("load",()=>
+{
+let long;
+let lat;
+if(navigator.geolocation)
+{
+    navigator.geolocation.getCurrentPosition((position)=>
+    {
+        long=position.coords.longitude;
+        lat=position.coords.latitude;
+        const api=`${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=17e7244f7199ae6d115a344f8012d9d2`
+        
+        fetch(api).then((response)=>
+        {
+            return response.json();
+        })
+        .then(data=>
+            {
+                const{name}=data;
+                const{feels_like}=data.main;
+                const{id,main}=data.weather[0];
+
+                loc.textContent=name;
+                climate.textContent=main;
+                tempvalue.textContent=Math.round(feels_like-273);
+            }
+            )
+    }
+    )}
+}
+)
+
+
