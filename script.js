a9c3cff6ea4fa9f34230c61e83caa1d92a76235a
prashant462 <prashantmalhotra462@gmail.com>
2021-01-06 17:18:25 +0530

    For any city update

--- script.js
+++ script.js
@@ -7,6 +7,61 @@ const searchInput=document.getElementById("search-input");
 const searchButton=document.getElementById("search-button");
 const proxy="https://cors-anywhere.herokuapp.com/";
 
+searchButton.addEventListener("click",(e)=>
+{
+    e.preventDefault();
+    getWeather(searchInput.value);
+    searchInput.value="";
+
+})
+const getWeather=async(city)=>
+{
+    try
+    {
+        const api1=`${proxy}api.openweathermap.org/data/2.5/weather?q=${city}&appid=`+key;
+        fetch(api1).then((response)=>
+        {
+            return response.json();
+        })
+        .then(weatherData=>
+            {
+                const{name}=weatherData;
+                const{feels_like}=weatherData.main;
+                const{id,main}=weatherData.weather[0];
+                loc.textContent=name;
+                climate.textContent=main;
+                tempvalue.textContent=Math.round(feels_like-273);
+                if (id<300 && id>200)
+                {
+                    tempvalue.src="thunderstorm.svg";
+                }
+                else if (id<400 && id>300)
+                {
+                    tempvalue.src="cloud.png";
+                }
+                else if (id<600 && id>500)
+                {
+                    tempvalue.src="rain.svg";
+                }
+                else if (id<700 && id>600)
+                {
+                    tempvalue.src="atmosphere.svg";
+                }
+                else if (id<800 && id>700)
+                {
+                    tempvalue.src="clouds-and-sun.svg";
+                }
+                if (id==800)
+                {
+                    tempvalue.src="sun.png";
+                }
+            })
+    }
+    catch(error)
+    {
+        alert("City Not Found");
+    }
+}
 window.addEventListener("load",()=>
 {
 let long;
